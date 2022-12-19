ec5d448622e81eeae5d688a971119e7f739846ff
HyesungKum <rmagptjd77@gmail.com>
2022-12-19 15:01:23 +0900

    test

--- Assets/Editor/Jenkins.cs
+++ Assets/Editor/Jenkins.cs
@@ -41,6 +41,11 @@ public class Jenkins
     [UnityEditor.MenuItem("TestBuild/ProjectBuild", false, 1)]
     static void PerformBuild()
     {
+        if (!Directory.Exists(Directory.GetCurrentDirectory() + "/Build/"))
+        {
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Build/");
+        }
+
         string target_filename = APP_NAME + ".exe";
         string[] scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
 
@@ -49,6 +54,6 @@ public class Jenkins
 
     static void GenericBuild(string[] scenes, string target_filename, BuildTarget build_target, BuildOptions build_options)
     {
-        BuildPipeline.BuildPlayer(scenes, target_filename, build_target, build_options);
+        BuildPipeline.BuildPlayer(scenes, Directory.GetCurrentDirectory() + "/Build/" + target_filename, build_target, build_options);
     }
 }
