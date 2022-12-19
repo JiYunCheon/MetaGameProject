83cd974da8beca822e3231e53cf3e79acb304f54
HyesungKum <rmagptjd77@gmail.com>
2022-12-19 15:15:35 +0900

    test

--- Assets/Editor/Jenkins.cs
+++ Assets/Editor/Jenkins.cs
@@ -8,17 +8,17 @@ using System;
 public class Jenkins
 {
     static string[] SCENES = FindEnabledEditorScenes();
-    static string APP_NAME = "MetaProgramming_2";
+    static string APP_NAME = "MetaGameProject";
 
     [UnityEditor.MenuItem("TestBuild/ProjectBuild", false, 1)]
     static void PerformBuild()
     {
-        if (!Directory.Exists(Directory.GetCurrentDirectory() + "/Build/"))
-        {
-            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Build/");
-        }
+        //if (!Directory.Exists(Directory.GetCurrentDirectory() + "/Build/"))
+        //{
+        //    Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Build/");
+        //}
 
-        string target_filename = APP_NAME + ".exe";
+        string target_filename = "Build/" + APP_NAME + ".exe";
         SCENES = FindEnabledEditorScenes();
 
         GenericBuild(SCENES, target_filename, BuildTarget.StandaloneWindows64, BuildOptions.None);
